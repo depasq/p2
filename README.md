c447e60687a6e11e8f426db773b46c4b7a696d33
Joseph DePasquale <jdepasquale@cfa.harvard.edu>
2015-09-16 20:05:19 -0400

    initialize repository

---
+++ README.md
@@ -0,0 +1,17 @@
+# CSCI E-15 Project 2 - XKCD-style Password Generator
+
+## Live URL
+<http://p2.dwa15.jdepasquale.com>
+
+## Description
+A random password generator inspired by [this](http://xkcd.com/936/) XKCD comic.
+
+## Demo
+Coming soon...
+
+## Details for teaching team
+This page uses PureCSS with additional custom formatting contained in css/p2.css
+
+## Outside code
+* Pure CSS: http://yui.yahooapis.com/pure/0.5.0/pure-min.css
+* Pure CSS Side Menu: css/side-menu.css
---
+++ index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="favicon-rocket.ico">
+  <title>Joe DePasquale | Portfolio</title>
+  <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css" type='text/css'>
+  <link rel="stylesheet" href="css/side-menu.css" type='text/css'>
+  <link rel="stylesheet" href="css/p1.css" type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css">
+
+</head>
+
+<body>
+
+  <div id="layout">
+    <!-- Menu toggle -->
+    <a href="#menu" id="menuLink" class="menu-link">
+      <!-- Hamburger icon -->
+      <span></span>
+    </a>
+
+    <div id="menu">
+      <div class="pure-menu">
+        <a class="pure-menu-heading" href="#">DWA15</a>
+        <ul class="pure-menu-list">
+          <li class="pure-menu-item"><a href="http://p1.dwa15.jdepasquale.com/" class="pure-menu-link">Project 1</a>
+            <ul class="pure-menu-children">
+              <li class="pure-menu-item ghub"><a href="https://github.com/depasq/p1" target="_blank" class="pure-menu-link">GitHub</a></li>
+            </ul>
+          </li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Project 2</a>
+            <ul class="pure-menu-children">
+              <li class="pure-menu-item ghub"><a href="https://github.com/depasq" target="_blank" class="pure-menu-link">GitHub</a></li>
+            </ul>
+          </li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Project 3</a>
+            <ul class="pure-menu-children">
+              <li class="pure-menu-item ghub"><a href="https://github.com/depasq" target="_blank" class="pure-menu-link">GitHub</a></li>
+            </ul>
+          </li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Project 4</a>
+            <ul class="pure-menu-children">
+              <li class="pure-menu-item ghub"><a href="https://github.com/depasq" target="_blank" class="pure-menu-link">GitHub</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div id="main">
+      <div class="header">
+        <h1>Joe DePasquale</h1>
+        <div class="pageTitleBG">
+          <div class="image-cropper">
+            <img src="img/head-shot.png" alt="Headshot">
+          </div>
+          <div id="linksPos">
+            <ul class="links">
+              <li><i class="fa fa-home"></i><a href='http://chandra.si.edu/' target="_blank">chandra.si.edu</a>
+              </li>
+              <li><i class="fa fa-github"></i><a href='https://github.com/depasq' target="_blank">depasq</a>
+              </li>
+              <li><i class="fa fa-twitter"></i><a href='https://twitter.com/astr0Pixel' target="_blank">@astr0Pixel</a>
+              </li>
+              <li><i class="fa fa-envelope"></i><a href='mailto:jdepasquale@cfa.harvard.edu'>depasq@cfa</a>
+              </li>
+              <li><i class="fa fa-music"></i><a href='http://www.ridingshotgun.com' target="_blank">Riding Shotgun</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="content">
+        <h2 class="content-subhead">About Me</h2>
+        <p>
+          I work as a Visualization Scientist in public outreach for NASA's Chandra X-ray Observatory at the Smithsonian Astrophysical Observatory. If you've ever seen a space image with X-ray data in it, you've probably seen my work. For as long as I can remember, I've been fascinated by
+          the immensity and complexity of the Universe and our search for meaning within it.
+        </p>
+
+        <div class="pure-g">
+          <div class="pure-u-1-4">
+            <img class="pure-img-responsive" src="img/sgra.jpg" alt="Peyto Lake">
+          </div>
+          <div class="pure-u-1-4">
+            <img class="pure-img-responsive" src="img/macsj0416.jpg" alt="Train">
+          </div>
+          <div class="pure-u-1-4">
+            <img class="pure-img-responsive" src="img/tycho.jpg" alt="T-Shirt Store">
+          </div>
+          <div class="pure-u-1-4">
+            <img class="pure-img-responsive" src="img/cena.jpg" alt="Mountain">
+          </div>
+        </div>
+
+        <h2 class="content-subhead">Dynamic Web Applications</h2>
+        <p>
+          More and more of the projects that I work on involve some web-based component. Perhaps my most favorite project is the <a href="http://chandra.si.edu/photo/map/index.html" target="_blank">Chandra Sky Map</a>, an interactive map for exploring
+          Chandra's press release imagery built in javascript using the D3 data visualization library. I'm interested in expanding my current skill set and learning the latest and greatest web technologies to provide new avenues for innovation and interactivity
+          in astronomical public outreach.
+        </p>
+
+        <h2 class="content-subhead">Experience</h2>
+        <p>
+          I've worked with HTML for the last 18 years, HTML+CSS for the last 12, and HTML+CSS+JS for the last 3 years. I have been coding for almost 20 years and am proficient in Python, Perl, and IDL with some experience in C++ and FORTRAN. I work in Mac OS X.
+          When I'm not working with images of the cosmos, I play guitar in a local, 3-piece instrumental rock band.
+        </p>
+      </div>
+    </div>
+    <script src="js/ui.js"></script>
+  </div>
+</body>
+
+</html>
