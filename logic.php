de3ab82a1e5d0a071df991a81a57cbf455f01a26
Joseph DePasquale <jdepasquale@cfa.harvard.edu>
2015-09-17 21:37:18 -0400

    test commit with new laptop

---
+++ logic.php
@@ -0,0 +1,5 @@
+<?php
+/* setup an array as proof of concept for the password
+generator */
+
+  $words = array('monkey', 'bar', 'apple', 'pizza', 'pole', 'board', 'pedal', 'guitar', 'computer', 'laptop', 'shift');
